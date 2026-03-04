1c0c48f107d2f6856af0a7c3283972b38ec9cf67
Raptorofthecage <thevelociraptors@hotmail.com>
2026-03-04 10:18:46 +0100

    camelCase

--- store.js
+++ store.js
@@ -3,7 +3,7 @@ let date = new Date()
 let dateNow = [date.getUTCDate(), date.getUTCMonth() + 1, date.getUTCFullYear()]
 let save
 let localData = localStorage.getItem("save")
-let storeGrid = document.querySelector(".storegrid")
+let storeGrid = document.querySelector(".storeGrid")
 let p = [0,1,2,3,4,5,6,7,8,9,10,11,12,
     13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29]
 try {
